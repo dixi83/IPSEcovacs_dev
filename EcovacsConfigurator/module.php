768be62e2cbaa8b221cc781951c25e044b5804a0
Martijn <martijn.diks@gmail.com>
2018-12-29 20:32:11 +0100

    Update module.php

--- EcovacsConfigurator/module.php
+++ EcovacsConfigurator/module.php
@@ -2,9 +2,11 @@
 
     require_once(__DIR__ . "/../libs/EcoVacsModule.php");
 
-    class Configurator extends IPSModule 
+    class EcovacsSplitter extends IPSModule 
     {
         // IPS functions needed for the Module:
+        
+        use Ecovacs
         
         public function Create(){
             //Never delete this line!
@@ -16,6 +18,15 @@
 			parent::ApplyChanges();	
             $this->RegisterVariableString ("AccountInfo", "AccountInfo","",0);
 		}
+        
+        public function TestAndSaveLogin($country,$httpServer,$xmppServer,$username,$password) {
+            $this->setAccountInfo($country, $httpServer, $xmppServer, $username, $password);
+            if($this->EVDB_HTTPS_Login()) {
+                echo "Login succesful and saved";
+            } else {
+                echo "Login failed, please check your entered account information";
+            }
+        }
     }
 
 ?>
