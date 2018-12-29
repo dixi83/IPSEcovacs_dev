ea365d23db48e0df275d39822ed2b1de147a49ee
Martijn <martijn.diks@gmail.com>
2018-12-29 21:35:47 +0100

    Update module.php

--- EcovacsConfigurator/module.php
+++ EcovacsConfigurator/module.php
@@ -4,30 +4,19 @@
 
     class EcovacsSplitter extends IPSModule 
     {
-        // IPS functions needed for the Module:
-        
         use EcovacsHTTP;
         
+        // IPS functions needed for the Module:
         public function Create(){
-            //Never delete this line!
-            parent::Create();
+            parent::Create(); //Never delete this line!
         }
         
         public function ApplyChanges(){
-			//Never delete this line!
-			parent::ApplyChanges();	
-            $this->RegisterVariableString ("AccountInfo", "AccountInfo","",0);
+			parent::ApplyChanges();	//Never delete this line!
+            
+            $this->RegisterVariableString ("AccountInfo", "AccountInfo");
 		}
         
-        
-        public function TestAndSaveLogin($country,$httpServer,$xmppServer,$username,$password) {
-            $this->setAccountInfo($country, $httpServer, $xmppServer, $username, $password);
-            if($this->EVDB_HTTPS_Login()) {
-                echo "Login succesful and saved";
-            } else {
-                echo "Login failed, please check your entered account information";
-            }
-        }
     }
 
 ?>
