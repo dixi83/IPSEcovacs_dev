60e180fe747f8b35f55fc8d28af88d2b271e236e
Martijn <martijn.diks@gmail.com>
2018-12-29 19:33:07 +0100

    Update module.php

--- EcovacsConfigurator/module.php
+++ EcovacsConfigurator/module.php
@@ -16,16 +16,6 @@
 			parent::ApplyChanges();	
             $this->RegisterVariableString ("AccountInfo", "AccountInfo","",0);
 		}
-        
-        
-        public function TestAndSaveLogin($country,$httpServer,$xmppserver,$username,$password) {
-            EVDB_setAccountInfo($country, $httpServer, $xmppServer, $account, $password);
-            if(EVDB_HTTPS_Login()) {
-                echo "Login succesful and saved";
-            } else {
-                echo "Login failed, please check your entered account information";
-            }
-        }
     }
 
 ?>
