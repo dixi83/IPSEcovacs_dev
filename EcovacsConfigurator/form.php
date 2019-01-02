dc193e88d9e4ee0106b5115fa220a299db3d1ef5
Martijn <martijn.diks@gmail.com>
2019-01-02 22:31:02 +0100

    Update form.php

--- EcovacsConfigurator/form.php
+++ EcovacsConfigurator/form.php
@@ -5,48 +5,48 @@ $form = <<<EOT
     "actions": [
         {
             "type": "List",
-            "name": "Devices",
+            "name": "devices",
             "caption": "Devices",
             "rowCount": 5,
-            "add": true,
-            "delete": true,
+            "add": false,
+            "delete": false,
             "sort": {
-                "column": "Name",
+                "column": "RobotNr",
                 "direction": "ascending"
             },
-            "columns": [{
+            "columns": [
+            {
+                "caption": "#",
+                "name": "RobotNr", 
+                "width": "20px"
+            }, {
                 "caption": "InstanceID",
                 "name": "InstanceID", 
-                "width": "75px",
-                "add": 0,
-                "edit": {
-                    "type": "SelectInstance"
-                }
+                "width": "75px"
             }, {
                 "caption": "Name",
-                "name": "Name",
+                "name": "RobotName",
                 "width": "auto",
-                "add": ""
-            }, {
-                "caption": "State",
-                "name": "State",
-                "width": "40px",
-                "add": "New!"
-            }, {
-                "caption": "Temperature",
-                "name": "Temperature",
-                "width": "75px",
-                "add": 20.0,
                 "edit": {
-                    "type": "NumberSpinner",
-                    "digits": 2
+                    "type": "ValidationTextBox"
                 }
-            }],
+            }, {
+                "caption": "Device Serial (identifier)",
+                "name": "DeviceSerialNr",
+                "width": "150px",
+                "visible": true
+            }, {
+                "caption": "XMPPaddress",
+                "name": "XMPPaddress",
+                "visible": false
+            }
+            ],
             "values": [{
-                "InstanceID": 12435,
-                "Name": "ABCD",
-                "State": "OK!",
-                "Temperature": 23.31,
+                "InstanceID": 0,
+                "RobotNr": 0,
+                "RobotName": "ABCD",
+                "DeviceSerialNr": "KOJSDAS78IJ89H3E98",
+                "XMPPaddress": "ahsdkashjd@sadkjhsajdh.com/atom",
                 "rowColor": "#ff0000" //rot
             }]
         }
