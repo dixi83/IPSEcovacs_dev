72008199508cbb1f02a54bdbe013eaf1b837f545
Martijn <martijn.diks@gmail.com>
2019-01-02 20:56:04 +0100

    Create xmpp.php

---
+++ libs/xmpp/xmpp.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EVlibs\xmpp;
+
+use EVlibs\xmpp\Options;
+use EVlibs\xmpp\Client;
+use EVlibs\xmpp\Protocol\Roster;
+use EVlibs\xmpp\Protocol\Presence;
+use EVlibs\xmpp\Protocol\Message;
+use EVlibs\xmpp\Connection;
+
+?>
