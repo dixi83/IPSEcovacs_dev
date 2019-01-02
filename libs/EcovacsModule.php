022e64b372b5b35ea76751db338e1b236b766aa3
Martijn <martijn.diks@gmail.com>
2019-01-02 20:27:40 +0100

    Update EcovacsModule.php

--- libs/EcovacsModule.php
+++ libs/EcovacsModule.php
@@ -2,11 +2,12 @@
 
 // include xmpp library
 //require_once('xmpp/Client.php');
-namespace EVlibs
+namespace EVlibs;
+
 use EVlibs\xmpp\Options;
 use EVlibs\xmpp\Client;
 use EVlibs\xmpp\Protocol\Roster;
-use EVlibs\EVlibs\xmpp\Protocol\Presence;
+use EVlibs\xmpp\Protocol\Presence;
 use EVlibs\xmpp\Protocol\Message;
 use EVlibs\xmpp\Connection;
 
