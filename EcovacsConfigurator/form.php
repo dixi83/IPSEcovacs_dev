bba5425280a22d2180dec84c5473e391efaed9ef
Martijn <martijn.diks@gmail.com>
2019-01-02 22:02:29 +0100

    Update form.php

--- EcovacsConfigurator/form.php
+++ EcovacsConfigurator/form.php
@@ -1,55 +1,57 @@
 <?php
 
 $form = <<<EOT
-"actions": [
-    {
-        "type": "List",
-        "name": "Devices",
-        "caption": "Devices",
-        "rowCount": 5,
-        "add": true,
-        "delete": true,
-        "sort": {
-            "column": "Name",
-            "direction": "ascending"
-        },
-        "columns": [{
-            "caption": "InstanceID",
-            "name": "InstanceID", 
-            "width": "75px",
-            "add": 0,
-            "edit": {
-                "type": "SelectInstance"
-            }
-        }, {
-            "caption": "Name",
-            "name": "Name",
-            "width": "auto",
-            "add": ""
-        }, {
-            "caption": "State",
-            "name": "State",
-            "width": "40px",
-            "add": "New!"
-        }, {
-            "caption": "Temperature",
-            "name": "Temperature",
-            "width": "75px",
-            "add": 20.0,
-            "edit": {
-                "type": "NumberSpinner",
-                "digits": 2
-            }
-        }],
-        "values": [{
-            "InstanceID": 12435,
-            "Name": "ABCD",
-            "State": "OK!",
-            "Temperature": 23.31,
-            "rowColor": "#ff0000" //rot
-        }]
-    }
-]
+{
+    "actions": [
+        {
+            "type": "List",
+            "name": "Devices",
+            "caption": "Devices",
+            "rowCount": 5,
+            "add": true,
+            "delete": true,
+            "sort": {
+                "column": "Name",
+                "direction": "ascending"
+            },
+            "columns": [{
+                "caption": "InstanceID",
+                "name": "InstanceID", 
+                "width": "75px",
+                "add": 0,
+                "edit": {
+                    "type": "SelectInstance"
+                }
+            }, {
+                "caption": "Name",
+                "name": "Name",
+                "width": "auto",
+                "add": ""
+            }, {
+                "caption": "State",
+                "name": "State",
+                "width": "40px",
+                "add": "New!"
+            }, {
+                "caption": "Temperature",
+                "name": "Temperature",
+                "width": "75px",
+                "add": 20.0,
+                "edit": {
+                    "type": "NumberSpinner",
+                    "digits": 2
+                }
+            }],
+            "values": [{
+                "InstanceID": 12435,
+                "Name": "ABCD",
+                "State": "OK!",
+                "Temperature": 23.31,
+                "rowColor": "#ff0000" //rot
+            }]
+        }
+    ]
+}
 EOT;
 
 ?>
