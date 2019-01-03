b9073f9e7df3c67671939f22708f6729995594a4
Martijn <martijn.diks@gmail.com>
2019-01-03 22:56:20 +0100

    Update module.php

--- EcovacsConfigurator/module.php
+++ EcovacsConfigurator/module.php
@@ -29,24 +29,6 @@ class EcovacsConfigurator extends IPSModule
         return $form;
     }
     
-    public function FindParentSplitter() {
-        $ids = IPS_GetInstanceListByModuleID("{8EB4291C-8EC8-4E10-B5D7-1F90CC37BD8D}");
-        
-        if(sizeof($ids) > 0) {
-            $hooks = json_decode(IPS_GetProperty($ids[0], "Hooks"), true);
-            $found = false;
-            foreach($ids as $index => $id) {
-                if($hook['Hook'] == $WebHook) {
-                    if($hook['TargetID'] == $this->InstanceID)
-                        return;
-                    $hooks[$index]['TargetID'] = $this->InstanceID;
-                    $found = true;
-                }
-            }
-        } else {
-            return false;
-        }
-    }
 }
     
 ?>
