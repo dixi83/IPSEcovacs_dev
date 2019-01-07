e0c2e8106fae8fc81a20315319e6d33666aa8a87
Martijn <martijn.diks@gmail.com>
2019-01-07 12:40:52 +0100

    Update loadXMPP.php

--- libs/loadXMPP.php
+++ libs/loadXMPP.php
@@ -20,4 +20,44 @@ class AutoloaderXMPP
     }
 }
 
+class AutoloaderPsr
+{
+    private $namespace;
+    public function __construct($namespace = null)
+    {
+        $this->namespace = $namespace;
+    }
+    public function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+    public function loadClass($className)
+    {
+        $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+}
+
+class AutoloaderMonolog
+{
+    private $namespace;
+    public function __construct($namespace = null)
+    {
+        $this->namespace = $namespace;
+    }
+    public function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+    public function loadClass($className)
+    {
+        $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+}
+
 ?>
